937c79218fd61009bf38c7302df37d82d62524d4
kimia rahimi <kimiarahimi76@gmail.com>
2025-05-03 15:32:37 +0430

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-
-
 import './App.css'
 import Card from './component/Card'
 
 function App() {
   return (
-    <>
-    <body>
-      <div className='card-container'>
-        <Card title="The Alchemist" rate={5} isCool={true}/>
-        <Card title="Pride and Prejudice" rate={4} isCool={true}/>
-        <Card title="Thinking, fast and slow" rate={5} isCool={true}/>
+
+    <main >
+      <div className='pattern'/>
+      <div className='wrapper'>
+        <header>
+          <img src='./bookCover.png'/>
+          <h1>Unlock your next <spann className='text-gradient'>adventure</spann>… Type a title!</h1>
+        </header>
       </div>
-      </body>
-    </>
+    </main>
+
   )
 }
 
