cb2502ccbb3e0bc7edfdc84e8880d3b23a583eb1
kimia rahimi <kimiarahimi76@gmail.com>
2025-05-03 18:05:12 +0430

    edit style

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ function App() {
     <main >
       <div className='pattern'/>
       <div className='wrapper'>
+        <h1>Readora</h1>
+      <img className='angle' src='./angle.png'/>
         <header>
           <img src='./bookCover.png'/>
-          <h1>Unlock your next <spann className='text-gradient'>adventure</spann>… Type a title!</h1>
+          <h2 className='search-text'>Unlock your next <spann className='text-gradient'>adventure</spann>… Type a title!</h2>
         </header>
       </div>
     </main>
