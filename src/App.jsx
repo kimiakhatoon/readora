45f58afdacda36fe85f598eac530d6b7f64af78b
kimia rahimi <kimiarahimi76@gmail.com>
2025-05-03 18:30:25 +0430

    update vite version

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
       <div className='pattern'/>
       <div className='wrapper'>
         <h1>Readora</h1>
-      <img className='angle' src='./angle.png'/>
         <header>
           <img src='./bookCover.png'/>
           <h2 className='search-text'>Unlock your next <spann className='text-gradient'>adventure</spann>… Type a title!</h2>
